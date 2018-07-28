ce62bbe527b2bba0cde5f2b12f13869ca0935d79
MrJackWilson <jack@brickwall.uk.com>
2018-07-28 16:32:07 +0100

    Setup basic electron app

---
+++ main.js
@@ -0,0 +1,16 @@
+const electron = require('electron');
+const { app, BrowserWindow } = require('electron');
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({ width: 1024, height: 768 });
+
+    win.loadFile('index.html');
+
+    win.on('closed', () => {
+        win = null;
+    });
+}
+
+app.on('ready', createWindow);
